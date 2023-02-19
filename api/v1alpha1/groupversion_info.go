f35fcc71ced43501f37a1993bd6a98c31bc1c4c5
Anish Asthana <anishasthana1@gmail.com>
2023-02-19 18:43:25 -0500

    Update CRD specification
    Signed-off-by: Anish Asthana <anishasthana1@gmail.com>

--- api/v1alpha1/groupversion_info.go
+++ api/v1alpha1/groupversion_info.go
@@ -15,8 +15,8 @@ limitations under the License.
 */
 
 // Package v1alpha1 contains API Schema definitions for the codeflare v1alpha1 API group
-//+kubebuilder:object:generate=true
-//+groupName=codeflare.codeflare.dev
+// +kubebuilder:object:generate=true
+// +groupName=codeflare.codeflare.dev
 package v1alpha1
 
 import (
