93024051b39edd36febe06bb23cde7bfa013539f
Anish Asthana <anishasthana1@gmail.com>
2023-02-22 13:51:14 -0500

    Update CodeFlare controller to create MCAD configmap
    Signed-off-by: Anish Asthana <anishasthana1@gmail.com>

---
+++ controllers/multi_cluster_app_dispatcher.go
@@ -0,0 +1,28 @@
+package controllers
+
+import (
+	"context"
+	codeflarev1alpha1 "github.com/project-codeflare/codeflare-operator/api/v1alpha1"
+	ctrl "sigs.k8s.io/controller-runtime"
+)
+
+var (
+	foo = codeflarev1alpha1.MCADSpec{EnableMonitoring: true}
+)
+
+var multiClusterAppDispatcherTemplates = []string{
+	"mcad/configmap.yaml.tmpl",
+}
+
+func (r *MCADReconciler) ReconcileMCAD(ctx context.Context, mcad *codeflarev1alpha1.MCAD, req ctrl.Request, params *MCADParams) error {
+
+	for _, template := range multiClusterAppDispatcherTemplates {
+		err := r.Apply(mcad, params, template)
+		if err != nil {
+			return err
+		}
+	}
+
+	r.Log.Info("Finished applying MultiClusterAppDispatcher Resources")
+	return nil
+}
