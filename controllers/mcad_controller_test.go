0089e9effdd62eb23845690fc2de59398e7d9cf4
Anish Asthana <anishasthana1@gmail.com>
2023-02-23 16:49:22 -0500

    Add working unit tests
    Signed-off-by: Anish Asthana <anishasthana1@gmail.com>

---
+++ controllers/mcad_controller_test.go
@@ -0,0 +1,55 @@
+package controllers
+
+import (
+	"context"
+	mfc "github.com/manifestival/controller-runtime-client"
+	mf "github.com/manifestival/manifestival"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	codeflarev1alpha1 "github.com/project-codeflare/codeflare-operator/api/v1alpha1"
+	"github.com/project-codeflare/codeflare-operator/controllers/testutil"
+
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/types"
+)
+
+const (
+	mcadCRCase1    = "./testdata/mcad_test_cases/case_1.yaml"
+	mcadConfigMap1 = "./testdata/mcad_test_results/case_1/configmap.yaml"
+)
+
+func deployMCAD(ctx context.Context, path string, opts mf.Option) {
+	mcad := &codeflarev1alpha1.MCAD{}
+	err := convertToStructuredResource(path, mcad, opts)
+	Expect(err).NotTo(HaveOccurred())
+	Expect(k8sClient.Create(ctx, mcad)).Should(Succeed())
+}
+
+func compareConfigMaps(path string, opts mf.Option) {
+	expectedConfigMap := &v1.ConfigMap{}
+	Expect(convertToStructuredResource(path, expectedConfigMap, opts)).NotTo(HaveOccurred())
+
+	actualConfigMap := &v1.ConfigMap{}
+	Eventually(func() error {
+		namespacedNamed := types.NamespacedName{Name: expectedConfigMap.Name, Namespace: WorkingNamespace}
+		return k8sClient.Get(ctx, namespacedNamed, actualConfigMap)
+	}, timeout, interval).ShouldNot(HaveOccurred())
+
+	Expect(testutil.ConfigMapsAreEqual(*expectedConfigMap, *actualConfigMap)).Should(BeTrue())
+
+}
+
+var _ = Describe("The MCAD Controller", func() {
+	client := mfc.NewClient(k8sClient)
+	opts := mf.UseClient(client)
+	ctx := context.Background()
+
+	Context("In a namespace, when a blank MCAD Custom Resource is deployed", func() {
+
+		It("It should create a configmap", func() {
+			deployMCAD(ctx, mcadCRCase1, opts)
+			compareConfigMaps(mcadConfigMap1, opts)
+		})
+	})
+
+})
--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -17,41 +17,61 @@ limitations under the License.
 package controllers
 
 import (
+	"context"
+	"go.uber.org/zap/zapcore"
 	"path/filepath"
 	"testing"
+	"time"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
+	mf "github.com/manifestival/manifestival"
+	codeflarev1alpha1 "github.com/project-codeflare/codeflare-operator/api/v1alpha1"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/client-go/kubernetes/scheme"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
+	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
-	"sigs.k8s.io/controller-runtime/pkg/envtest/printer"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
-
-	codeflarev1alpha1 "github.com/project-codeflare/codeflare-operator/api/v1alpha1"
 	//+kubebuilder:scaffold:imports
 )
 
 // These tests use Ginkgo (BDD-style Go testing framework). Refer to
 // http://onsi.github.io/ginkgo/ to learn more about Ginkgo.
 
-var cfg *rest.Config
-var k8sClient client.Client
-var testEnv *envtest.Environment
+var (
+	cfg       *rest.Config
+	k8sClient client.Client
+	testEnv   *envtest.Environment
+	ctx       context.Context
+	cancel    context.CancelFunc
+)
+
+const (
+	WorkingNamespace = "default"
+	timeout          = time.Second * 30
+	interval         = time.Millisecond * 10
+)
 
 func TestAPIs(t *testing.T) {
 	RegisterFailHandler(Fail)
 
-	RunSpecsWithDefaultAndCustomReporters(t,
-		"Controller Suite",
-		[]Reporter{printer.NewlineReporter{}})
+	RunSpecs(t, "Controller Suite")
 }
 
 var _ = BeforeSuite(func() {
-	logf.SetLogger(zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true)))
+	ctx, cancel = context.WithCancel(context.TODO())
+
+	// Initialize logger
+	opts := zap.Options{
+		Development: true,
+		TimeEncoder: zapcore.TimeEncoderOfLayout(time.RFC3339),
+	}
+	logf.SetLogger(zap.New(zap.WriteTo(GinkgoWriter), zap.UseFlagOptions(&opts)))
 
 	By("bootstrapping test environment")
 	testEnv = &envtest.Environment{
@@ -65,19 +85,73 @@ var _ = BeforeSuite(func() {
 	Expect(err).NotTo(HaveOccurred())
 	Expect(cfg).NotTo(BeNil())
 
-	err = codeflarev1alpha1.AddToScheme(scheme.Scheme)
-	Expect(err).NotTo(HaveOccurred())
-
+	// Register API objects
+	utilruntime.Must(clientgoscheme.AddToScheme(scheme.Scheme))
+	utilruntime.Must(codeflarev1alpha1.AddToScheme(scheme.Scheme))
 	//+kubebuilder:scaffold:scheme
 
+	// Initialize Kubernetes client
 	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
 	Expect(err).NotTo(HaveOccurred())
 	Expect(k8sClient).NotTo(BeNil())
 
+	// Setup controller manager
+	mgr, err := ctrl.NewManager(cfg, ctrl.Options{
+		Scheme:             scheme.Scheme,
+		LeaderElection:     false,
+		MetricsBindAddress: "0",
+	})
+	Expect(err).NotTo(HaveOccurred())
+
+	err = (&MCADReconciler{
+		Client:        k8sClient,
+		Log:           ctrl.Log.WithName("controllers").WithName("mcad-controller"),
+		Scheme:        scheme.Scheme,
+		TemplatesPath: "../config/internal/",
+	}).SetupWithManager(mgr)
+	Expect(err).ToNot(HaveOccurred())
+
+	// Start the manager
+	go func() {
+		defer GinkgoRecover()
+		err = mgr.Start(ctx)
+		Expect(err).ToNot(HaveOccurred(), "Failed to run manager")
+	}()
+
 }, 60)
 
 var _ = AfterSuite(func() {
+	// Give some time to allow workers to gracefully shutdown
+	time.Sleep(5 * time.Second)
+	cancel()
 	By("tearing down the test environment")
+	time.Sleep(1 * time.Second)
 	err := testEnv.Stop()
 	Expect(err).NotTo(HaveOccurred())
 })
+
+// Cleanup resources to not contaminate between tests
+var _ = AfterEach(func() {
+	inNamespace := client.InNamespace(WorkingNamespace)
+	Expect(k8sClient.DeleteAllOf(context.TODO(), &codeflarev1alpha1.MCAD{}, inNamespace)).ToNot(HaveOccurred())
+
+})
+
+func convertToStructuredResource(path string, out interface{}, opts mf.Option) error {
+	m, err := mf.ManifestFrom(mf.Recursive(path), opts)
+	if err != nil {
+		return err
+	}
+	m, err = m.Transform(mf.InjectNamespace(WorkingNamespace))
+	if err != nil {
+		return err
+	}
+	if err != nil {
+		return err
+	}
+	err = scheme.Scheme.Convert(&m.Resources()[0], out, nil)
+	if err != nil {
+		return err
+	}
+	return nil
+}
