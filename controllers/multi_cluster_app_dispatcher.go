ed5ee90601e70266a0b9651df6308e7e7b4dd562
Anish Asthana <anishasthana1@gmail.com>
2023-02-28 11:45:34 -0500

    Move utility functions around, update tests
    Signed-off-by: Anish Asthana <anishasthana1@gmail.com>

--- controllers/multi_cluster_app_dispatcher.go
+++ controllers/multi_cluster_app_dispatcher.go
@@ -11,7 +11,7 @@ var multiClusterAppDispatcherTemplates = []string{
 	"mcad/service.yaml.tmpl",
 	"mcad/serviceaccount.yaml.tmpl",
 	"mcad/service.yaml.tmpl",
-	"mcad/rolebinding_custom-metrics-auth-reader.yaml.tmpl",
+	"mcad/rolebinding_custom-metrics-auth-reader.yaml.tmpl", //Confirm is this rolebinding is actually required by MCAD
 }
 
 func (r *MCADReconciler) ReconcileMCAD(ctx context.Context, mcad *codeflarev1alpha1.MCAD, req ctrl.Request, params *MCADParams) error {
