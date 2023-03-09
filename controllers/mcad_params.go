c1a66e09846749a9f692e7c872b60727e6088bb3
MichaelClifford <mcliffor@redhat.com>
2023-03-09 16:36:08 -0500

    allow resource specification in MCAD CR

--- controllers/mcad_params.go
+++ controllers/mcad_params.go
@@ -1,21 +1,38 @@
 package controllers
 
 import (
+	"encoding/json"
+
 	mf "github.com/manifestival/manifestival"
 	mcadv1alpha1 "github.com/project-codeflare/codeflare-operator/api/v1alpha1"
+	v1 "k8s.io/api/core/v1"
 )
 
 type MCADParams struct {
-	Name               string
-	Namespace          string
-	Owner              mf.Owner
-	EnableMonitoring   bool
-	MultiCluster       bool
-	DispatcherMode     bool
-	PreemptionEnabled  bool
-	AgentConfigs       string
-	QuotaRestURL       string
-	PodCreationTimeout int
+	Name                string
+	Namespace           string
+	Owner               mf.Owner
+	EnableMonitoring    bool
+	MultiCluster        bool
+	DispatcherMode      bool
+	PreemptionEnabled   bool
+	AgentConfigs        string
+	QuotaRestURL        string
+	PodCreationTimeout  int
+	ControllerResources ControllerResources
+}
+
+type ControllerResources struct {
+	v1.ResourceRequirements
+}
+
+func (c *ControllerResources) String() string {
+	raw, err := json.Marshal(c)
+	if err != nil {
+		return "{}"
+	} else {
+		return string(raw)
+	}
 }
 
 // ExtractParams is currently a straight-up copy. We can add in more complex validation at a later date
@@ -30,6 +47,7 @@ func (p *MCADParams) ExtractParams(mcad *mcadv1alpha1.MCAD) error {
 	p.AgentConfigs = mcad.Spec.AgentConfigs
 	p.QuotaRestURL = mcad.Spec.QuotaRestURL
 	p.PodCreationTimeout = mcad.Spec.PodCreationTimeout
+	p.ControllerResources = ControllerResources{mcad.Spec.ControllerResources}
 
 	return nil
 }
