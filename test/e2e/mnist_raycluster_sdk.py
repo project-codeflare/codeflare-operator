9a96e455c507fa989130e4d2d763c5dd21540c1d
Antonin Stefanutti <antonin@stefanutti.fr>
2023-06-26 17:45:11 +0200

    test: Pass MNIST training with CodeFlare SDK on OpenShift

---
+++ test/e2e/mnist_raycluster_sdk.py
@@ -0,0 +1,65 @@
+import sys
+
+from time import sleep
+
+from torchx.specs.api import AppState, is_terminal
+
+from codeflare_sdk.cluster.cluster import Cluster, ClusterConfiguration
+from codeflare_sdk.job.jobs import DDPJobDefinition
+
+namespace = sys.argv[1]
+
+cluster = Cluster(ClusterConfiguration(
+    name='mnist',
+    namespace=namespace,
+    min_worker=1,
+    max_worker=1,
+    min_cpus='500m',
+    max_cpus=1,
+    min_memory=0.5,
+    max_memory=1,
+    gpu=0,
+    instascale=False,
+))
+
+cluster.up()
+
+cluster.status()
+
+cluster.wait_ready()
+
+cluster.status()
+
+cluster.details()
+
+jobdef = DDPJobDefinition(
+    name="mnist",
+    script="mnist.py",
+    scheduler_args={"requirements": "requirements.txt"},
+)
+job = jobdef.submit(cluster)
+
+done = False
+time = 0
+timeout = 300
+while not done:
+    status = job.status()
+    if is_terminal(status.state):
+        break
+    if not done:
+        print(status)
+        if timeout and time >= timeout:
+            raise TimeoutError(f"job has timed out after waiting {timeout}s")
+        sleep(5)
+        time += 5
+
+print(f"Job has completed: {status.state}")
+
+print(job.logs())
+
+cluster.down()
+
+if not status.state == AppState.SUCCEEDED:
+    exit(1)
+else:
+    exit(0)
--- test/e2e/mnist_raycluster_sdk_test.go
+++ test/e2e/mnist_raycluster_sdk_test.go
@@ -23,57 +23,119 @@ import (
 
 	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
+	rbacv1 "k8s.io/api/rbac/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
+
+	rayv1alpha1 "github.com/ray-project/kuberay/ray-operator/apis/ray/v1alpha1"
 
 	. "github.com/project-codeflare/codeflare-operator/test/support"
+	mcadv1beta1 "github.com/project-codeflare/multi-cluster-app-dispatcher/pkg/apis/controller/v1beta1"
 )
 
 func TestMNISTRayClusterSDK(t *testing.T) {
 	test := With(t)
 	test.T().Parallel()
 
-	test.T().Skip("Requires https://github.com/project-codeflare/codeflare-sdk/pull/146")
+	if !IsOpenShift(test) {
+		test.T().Skip("Requires https://github.com/project-codeflare/codeflare-sdk/pull/146")
+	}
 
 	// Create a namespace
 	namespace := test.NewTestNamespace()
 
-	// SDK script
-	sdk := &corev1.ConfigMap{
+	// Test configuration
+	configMap := &corev1.ConfigMap{
 		TypeMeta: metav1.TypeMeta{
 			APIVersion: corev1.SchemeGroupVersion.String(),
 			Kind:       "ConfigMap",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "mnist-raycluster-sdk",
+			Namespace: namespace.Name,
+		},
+		BinaryData: map[string][]byte{
+			// SDK script
+			"mnist_raycluster_sdk.py": ReadFile(test, "mnist_raycluster_sdk.py"),
+			// pip requirements
+			"requirements.txt": ReadFile(test, "requirements.txt"),
+			// MNIST training script
+			"mnist.py": ReadFile(test, "mnist.py"),
+		},
+		Immutable: Ptr(true),
+	}
+	configMap, err := test.Client().Core().CoreV1().ConfigMaps(namespace.Name).Create(test.Ctx(), configMap, metav1.CreateOptions{})
+	test.Expect(err).NotTo(HaveOccurred())
+	test.T().Logf("Created ConfigMap %s/%s successfully", configMap.Namespace, configMap.Name)
+
+	// SDK client RBAC
+	serviceAccount := &corev1.ServiceAccount{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: corev1.SchemeGroupVersion.String(),
+			Kind:       "ServiceAccount",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "sdk-user",
+			Namespace: namespace.Name,
+		},
+	}
+	serviceAccount, err = test.Client().Core().CoreV1().ServiceAccounts(namespace.Name).Create(test.Ctx(), serviceAccount, metav1.CreateOptions{})
+	test.Expect(err).NotTo(HaveOccurred())
+
+	role := &rbacv1.Role{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: rbacv1.SchemeGroupVersion.String(),
+			Kind:       "Role",
 		},
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      "sdk",
 			Namespace: namespace.Name,
 		},
-		BinaryData: map[string][]byte{
-			"sdk.py": ReadFile(test, "sdk.py"),
+		Rules: []rbacv1.PolicyRule{
+			{
+				Verbs:     []string{"get", "create", "delete", "list", "patch", "update"},
+				APIGroups: []string{mcadv1beta1.GroupName},
+				Resources: []string{"appwrappers"},
+			},
+			{
+				Verbs:     []string{"get", "list"},
+				APIGroups: []string{rayv1alpha1.GroupVersion.Group},
+				Resources: []string{"rayclusters", "rayclusters/status"},
+			},
+			{
+				Verbs:     []string{"get", "list"},
+				APIGroups: []string{"route.openshift.io"},
+				Resources: []string{"routes"},
+			},
 		},
-		Immutable: Ptr(true),
 	}
-	sdk, err := test.Client().Core().CoreV1().ConfigMaps(namespace.Name).Create(test.Ctx(), sdk, metav1.CreateOptions{})
+	role, err = test.Client().Core().RbacV1().Roles(namespace.Name).Create(test.Ctx(), role, metav1.CreateOptions{})
 	test.Expect(err).NotTo(HaveOccurred())
-	test.T().Logf("Created ConfigMap %s/%s successfully", sdk.Namespace, sdk.Name)
 
-	// pip requirements
-	requirements := &corev1.ConfigMap{
+	roleBinding := &rbacv1.RoleBinding{
 		TypeMeta: metav1.TypeMeta{
-			APIVersion: corev1.SchemeGroupVersion.String(),
-			Kind:       "ConfigMap",
+			APIVersion: rbacv1.SchemeGroupVersion.String(),
+			Kind:       "RoleBinding",
 		},
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      "requirements",
-			Namespace: namespace.Name,
+			Name: "sdk",
 		},
-		BinaryData: map[string][]byte{
-			"requirements.txt": ReadFile(test, "requirements.txt"),
+		RoleRef: rbacv1.RoleRef{
+			APIGroup: rbacv1.SchemeGroupVersion.Group,
+			Kind:     "Role",
+			Name:     role.Name,
 		},
-		Immutable: Ptr(true),
+		Subjects: []rbacv1.Subject{
+			{
+				Kind:      "ServiceAccount",
+				APIGroup:  corev1.SchemeGroupVersion.Group,
+				Name:      serviceAccount.Name,
+				Namespace: serviceAccount.Namespace,
+			},
+		},
 	}
-	requirements, err = test.Client().Core().CoreV1().ConfigMaps(namespace.Name).Create(test.Ctx(), requirements, metav1.CreateOptions{})
+	_, err = test.Client().Core().RbacV1().RoleBindings(namespace.Name).Create(test.Ctx(), roleBinding, metav1.CreateOptions{})
 	test.Expect(err).NotTo(HaveOccurred())
-	test.T().Logf("Created ConfigMap %s/%s successfully", requirements.Namespace, requirements.Name)
 
 	job := &batchv1.Job{
 		TypeMeta: metav1.TypeMeta{
@@ -92,54 +154,62 @@ func TestMNISTRayClusterSDK(t *testing.T) {
 				Spec: corev1.PodSpec{
 					Containers: []corev1.Container{
 						{
-							Name:    "sdk",
+							Name:    "test",
 							Image:   "quay.io/opendatahub/notebooks:jupyter-minimal-ubi8-python-3.8-4c8f26e",
-							Command: []string{"/bin/sh", "-c", "pip install -r /test/runtime/requirements.txt && python /test/job/sdk.py"},
+							Command: []string{"/bin/sh", "-c", "pip install codeflare-sdk==0.4.4 && cp /test/* . && python mnist_raycluster_sdk.py" + " " + namespace.Name},
 							VolumeMounts: []corev1.VolumeMount{
 								{
-									Name:      "sdk",
-									MountPath: "/test/job",
-								},
-								{
-									Name:      "requirements",
-									MountPath: "/test/runtime",
+									Name:      "test",
+									MountPath: "/test",
 								},
 							},
 						},
 					},
 					Volumes: []corev1.Volume{
 						{
-							Name: "sdk",
+							Name: "test",
 							VolumeSource: corev1.VolumeSource{
 								ConfigMap: &corev1.ConfigMapVolumeSource{
 									LocalObjectReference: corev1.LocalObjectReference{
-										Name: sdk.Name,
-									},
-								},
-							},
-						},
-						{
-							Name: "requirements",
-							VolumeSource: corev1.VolumeSource{
-								ConfigMap: &corev1.ConfigMapVolumeSource{
-									LocalObjectReference: corev1.LocalObjectReference{
-										Name: requirements.Name,
+										Name: configMap.Name,
 									},
 								},
 							},
 						},
 					},
-					RestartPolicy: corev1.RestartPolicyNever,
+					RestartPolicy:      corev1.RestartPolicyNever,
+					ServiceAccountName: serviceAccount.Name,
 				},
 			},
 		},
 	}
 	job, err = test.Client().Core().BatchV1().Jobs(namespace.Name).Create(test.Ctx(), job, metav1.CreateOptions{})
 	test.Expect(err).NotTo(HaveOccurred())
+	test.T().Logf("Created Job %s/%s successfully", job.Namespace, job.Name)
 
 	defer JobTroubleshooting(test, job)
 
-	test.T().Logf("Waiting for Job %s/%s to complete successfully", job.Namespace, job.Name)
-	test.Eventually(Job(test, job.Namespace, job.Name), TestTimeoutMedium).
-		Should(WithTransform(ConditionStatus(batchv1.JobComplete), Equal(corev1.ConditionTrue)))
+	test.T().Logf("Waiting for Job %s/%s to complete", job.Namespace, job.Name)
+	test.Eventually(Job(test, job.Namespace, job.Name), TestTimeoutLong).Should(
+		Or(
+			WithTransform(ConditionStatus(batchv1.JobComplete), Equal(corev1.ConditionTrue)),
+			WithTransform(ConditionStatus(batchv1.JobFailed), Equal(corev1.ConditionTrue)),
+		))
+
+	// Refresh the job to get the generated pod selector
+	job = GetJob(test, job.Namespace, job.Name)
+
+	// Get the job Pod
+	pods := GetPods(test, job.Namespace, metav1.ListOptions{
+		LabelSelector: labels.FormatLabels(job.Spec.Selector.MatchLabels)},
+	)
+	test.Expect(pods).To(HaveLen(1))
+
+	// Print the job logs
+	test.T().Logf("Printing Job %s/%s logs", job.Namespace, job.Name)
+	test.T().Log(GetPodLogs(test, &pods[0], corev1.PodLogOptions{}))
+
+	// Assert the job has completed successfully
+	test.T().Logf("Checking the Job %s/%s has completed successfully", job.Namespace, job.Name)
+	test.Expect(job).To(WithTransform(ConditionStatus(batchv1.JobComplete), Equal(corev1.ConditionTrue)))
 }
