02ee9e4076285e6442b31ce17068ea2d6c424d61
Antonin Stefanutti <antonin@stefanutti.fr>
2023-06-28 15:49:46 +0200

    test: Print Ray job logs after successful or failed completion

--- test/support/ray.go
+++ test/support/ray.go
@@ -21,7 +21,6 @@ import (
 
 	"github.com/onsi/gomega"
 
-	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	rayv1alpha1 "github.com/ray-project/kuberay/ray-operator/apis/ray/v1alpha1"
@@ -29,20 +28,28 @@ import (
 
 const RayJobDefaultClusterSelectorKey = "ray.io/cluster"
 
-func RayJob(t Test, namespace *corev1.Namespace, name string) func(g gomega.Gomega) *rayv1alpha1.RayJob {
+func RayJob(t Test, namespace, name string) func(g gomega.Gomega) *rayv1alpha1.RayJob {
 	return func(g gomega.Gomega) *rayv1alpha1.RayJob {
-		job, err := t.Client().Ray().RayV1alpha1().RayJobs(namespace.Name).Get(t.Ctx(), name, metav1.GetOptions{})
+		job, err := t.Client().Ray().RayV1alpha1().RayJobs(namespace).Get(t.Ctx(), name, metav1.GetOptions{})
 		g.Expect(err).NotTo(gomega.HaveOccurred())
 		return job
 	}
+}
+
+func GetRayJob(t Test, namespace, name string) *rayv1alpha1.RayJob {
+	t.T().Helper()
+	return RayJob(t, namespace, name)(t)
 }
 
 func RayJobStatus(job *rayv1alpha1.RayJob) rayv1alpha1.JobStatus {
 	return job.Status.JobStatus
 }
 
-func GetRayJobLogs(t Test, job *rayv1alpha1.RayJob) string {
+func GetRayJobLogs(t Test, namespace, name string) string {
 	t.T().Helper()
+
+	job := GetRayJob(t, namespace, name)
+
 	response := t.Client().Core().CoreV1().RESTClient().
 		Get().
 		AbsPath("/api/v1/namespaces", job.Namespace, "services", "http:"+job.Status.RayClusterName+"-head-svc:dashboard", "proxy", "api", "jobs", job.Status.JobId, "logs").
