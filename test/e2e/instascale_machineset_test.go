ea9ee6abcbbdccd08a72d204b75e7b3bdfd508e4
Fiona Waters <fiwaters6@gmail.com>
2023-10-16 12:53:18 +0100

    refactor to use aw.Name

--- test/e2e/instascale_machineset_test.go
+++ test/e2e/instascale_machineset_test.go
@@ -6,6 +6,8 @@ import (
 	. "github.com/onsi/gomega"
 	mcadv1beta1 "github.com/project-codeflare/multi-cluster-app-dispatcher/pkg/apis/controller/v1beta1"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
 	. "github.com/project-codeflare/codeflare-operator/test/support"
 )
 
@@ -29,13 +31,16 @@ func TestInstascaleMachineSet(t *testing.T) {
 		"mnist.py": ReadFile(test, "mnist.py"),
 	})
 
-	// look for machine set with aw name - expect to find it
-	test.Expect(GetMachineSets(test)).Should(ContainElement(WithTransform(MachineSetId, Equal("test-instascale"))))
-	// look for machine belonging to the machine set, there should be none
-	test.Expect(GetMachines(test, "test-instascale")).Should(BeEmpty())
-
 	// // Setup batch job and AppWrapper
-	_, aw, err := createInstaScaleJobAppWrapper(test, namespace, cm)
+	aw := instaScaleJobAppWrapper(test, namespace, cm)
+
+	// look for machine set with aw name - expect to find it
+	test.Expect(GetMachineSets(test)).Should(ContainElement(WithTransform(MachineSetId, Equal(aw.Name))))
+	// look for machine belonging to the machine set, there should be none
+	test.Expect(GetMachines(test, aw.Name)).Should(BeEmpty())
+
+	// apply AppWrapper to cluster
+	_, err := test.Client().MCAD().WorkloadV1beta1().AppWrappers(namespace.Name).Create(test.Ctx(), aw, metav1.CreateOptions{})
 	test.Expect(err).NotTo(HaveOccurred())
 
 	// assert that AppWrapper goes to "Running" state
@@ -43,13 +48,13 @@ func TestInstascaleMachineSet(t *testing.T) {
 		Should(WithTransform(AppWrapperState, Equal(mcadv1beta1.AppWrapperStateActive)))
 
 	// look for machine belonging to the machine set - expect to find it
-	test.Eventually(Machines(test, "test-instascale"), TestTimeoutLong).Should(HaveLen(1))
+	test.Eventually(Machines(test, aw.Name), TestTimeoutLong).Should(HaveLen(1))
 
 	// assert that the AppWrapper goes to "Completed" state
 	test.Eventually(AppWrapper(test, namespace, aw.Name), TestTimeoutMedium).
 		Should(WithTransform(AppWrapperState, Equal(mcadv1beta1.AppWrapperStateCompleted)))
 
 	// look for machine belonging to the machine set - there should be none
-	test.Eventually(Machines(test, "test-instascale"), TestTimeoutLong).Should(BeEmpty())
+	test.Eventually(Machines(test, aw.Name), TestTimeoutLong).Should(BeEmpty())
 
 }
