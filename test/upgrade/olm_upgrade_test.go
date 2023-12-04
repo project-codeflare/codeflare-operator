0afa252a9a4178cd9e75f1e87e12c6954f41c105
Srihari <svenkata@redhat.com>
2023-12-04 16:42:56 +0530

    Create e2e scenario covering upgrade during training

--- test/upgrade/olm_upgrade_test.go
+++ test/upgrade/olm_upgrade_test.go
@@ -22,29 +22,27 @@ import (
 	. "github.com/onsi/gomega"
 	. "github.com/project-codeflare/codeflare-common/support"
 	mcadv1beta1 "github.com/project-codeflare/multi-cluster-app-dispatcher/pkg/apis/controller/v1beta1"
-	rayv1 "github.com/ray-project/kuberay/ray-operator/apis/ray/v1"
 
 	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
-	rbacv1 "k8s.io/api/rbac/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	. "github.com/project-codeflare/codeflare-operator/test/e2e"
 )
 
 var (
-	namespaceName = "test-ns-olmupgrade"
+	namespaceName  = "test-ns-olmupgrade"
+	appwrapperName = "mnist"
+	jobName        = "mnist-job"
 )
 
-// Creates a Ray cluster
-func TestMNISTRayClusterUp(t *testing.T) {
-
+func TestMNISTCreateAppWrapper(t *testing.T) {
 	test := With(t)
 	test.T().Parallel()
 
 	// Create a namespace
 	namespace := CreateTestNamespaceWithName(test, namespaceName)
-	test.T().Logf("Created namespace %s successfully", namespace.Name)
 
 	// Delete namespace only if test failed
 	defer func() {
@@ -56,88 +54,54 @@ func TestMNISTRayClusterUp(t *testing.T) {
 	}()
 
 	// Test configuration
-	config := CreateConfigMap(test, namespace.Name, map[string][]byte{
-		// SDK script
-		"start_ray_cluster.py": ReadFile(test, "start_ray_cluster.py"),
-		// codeflare-sdk installation script
-		"install-codeflare-sdk.sh": ReadFile(test, "install-codeflare-sdk.sh"),
-	})
-
-	// Create RBAC, retrieve token for user with limited rights
-	policyRules := []rbacv1.PolicyRule{
-		{
-			Verbs:     []string{"get", "create", "delete", "list", "patch", "update"},
-			APIGroups: []string{mcadv1beta1.GroupName},
-			Resources: []string{"appwrappers"},
+	config := &corev1.ConfigMap{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: corev1.SchemeGroupVersion.String(),
+			Kind:       "ConfigMap",
 		},
-		{
-			Verbs:     []string{"get", "list"},
-			APIGroups: []string{rayv1.GroupVersion.Group},
-			Resources: []string{"rayclusters", "rayclusters/status"},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "mnist-mcad",
+			Namespace: namespace.Name,
 		},
-		{
-			Verbs:     []string{"get", "list"},
-			APIGroups: []string{"route.openshift.io"},
-			Resources: []string{"routes"},
+		BinaryData: map[string][]byte{
+			// pip requirements
+			"requirements.txt": ReadFile(test, "mnist_pip_requirements.txt"),
+			// MNIST training script
+			"mnist.py": ReadFile(test, "mnist.py"),
 		},
-		{
-			Verbs:     []string{"get", "list"},
-			APIGroups: []string{"networking.k8s.io"},
-			Resources: []string{"ingresses"},
-		},
+		Immutable: Ptr(true),
 	}
+	config, err := test.Client().Core().CoreV1().ConfigMaps(namespace.Name).Create(test.Ctx(), config, metav1.CreateOptions{})
+	test.Expect(err).NotTo(HaveOccurred())
+	test.T().Logf("Created ConfigMap %s/%s successfully", config.Namespace, config.Name)
 
-	// Create cluster wide RBAC, required for SDK OpenShift check
-	// TODO reevaluate once SDK change OpenShift detection logic
-	clusterPolicyRules := []rbacv1.PolicyRule{
-		{
-			Verbs:         []string{"get", "list"},
-			APIGroups:     []string{"config.openshift.io"},
-			Resources:     []string{"ingresses"},
-			ResourceNames: []string{"cluster"},
-		},
-	}
-
-	sa := CreateServiceAccount(test, namespace.Name)
-	role := CreateRole(test, namespace.Name, policyRules)
-	CreateRoleBinding(test, namespace.Name, sa, role)
-	clusterRole := CreateClusterRole(test, clusterPolicyRules)
-	CreateClusterRoleBinding(test, sa, clusterRole)
-
+	// Batch Job
 	job := &batchv1.Job{
 		TypeMeta: metav1.TypeMeta{
 			APIVersion: batchv1.SchemeGroupVersion.String(),
 			Kind:       "Job",
 		},
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      "sdk",
+			Name:      jobName,
 			Namespace: namespace.Name,
 		},
 		Spec: batchv1.JobSpec{
-			Completions:  Ptr(int32(1)),
-			Parallelism:  Ptr(int32(1)),
-			BackoffLimit: Ptr(int32(0)),
+			Completions: Ptr(int32(1)),
+			Parallelism: Ptr(int32(1)),
 			Template: corev1.PodTemplateSpec{
 				Spec: corev1.PodSpec{
 					Containers: []corev1.Container{
 						{
-							Name: "test",
-							// FIXME: switch to base Python image once the dependency on OpenShift CLI is removed
-							// See https://github.com/project-codeflare/codeflare-sdk/pull/146
-							Image: "quay.io/opendatahub/notebooks:jupyter-minimal-ubi8-python-3.8-4c8f26e",
+							Name:  "job",
+							Image: GetPyTorchImage(),
 							Env: []corev1.EnvVar{
-								{Name: "PYTHONUSERBASE", Value: "/workdir"},
-								{Name: "RAY_IMAGE", Value: GetRayImage()},
+								corev1.EnvVar{Name: "PYTHONUSERBASE", Value: "/workdir"},
 							},
-							Command: []string{"/bin/sh", "-c", "cp /test/* . && chmod +x install-codeflare-sdk.sh && ./install-codeflare-sdk.sh && python start_ray_cluster.py" + " " + namespace.Name},
+							Command: []string{"/bin/sh", "-c", "pip install -r /test/requirements.txt && torchrun /test/mnist.py"},
 							VolumeMounts: []corev1.VolumeMount{
 								{
 									Name:      "test",
 									MountPath: "/test",
-								},
-								{
-									Name:      "codeflare-sdk",
-									MountPath: "/codeflare-sdk",
 								},
 								{
 									Name:      "workdir",
@@ -145,16 +109,6 @@ func TestMNISTRayClusterUp(t *testing.T) {
 								},
 							},
 							WorkingDir: "/workdir",
-							SecurityContext: &corev1.SecurityContext{
-								AllowPrivilegeEscalation: Ptr(false),
-								SeccompProfile: &corev1.SeccompProfile{
-									Type: "RuntimeDefault",
-								},
-								Capabilities: &corev1.Capabilities{
-									Drop: []corev1.Capability{"ALL"},
-								},
-								RunAsNonRoot: Ptr(true),
-							},
 						},
 					},
 					Volumes: []corev1.Volume{
@@ -169,235 +123,84 @@ func TestMNISTRayClusterUp(t *testing.T) {
 							},
 						},
 						{
-							Name: "codeflare-sdk",
-							VolumeSource: corev1.VolumeSource{
-								EmptyDir: &corev1.EmptyDirVolumeSource{},
-							},
-						},
-						{
 							Name: "workdir",
 							VolumeSource: corev1.VolumeSource{
 								EmptyDir: &corev1.EmptyDirVolumeSource{},
 							},
 						},
 					},
-					RestartPolicy:      corev1.RestartPolicyNever,
-					ServiceAccountName: sa.Name,
+					RestartPolicy: corev1.RestartPolicyNever,
 				},
 			},
 		},
 	}
-	if GetClusterType(test) == KindCluster {
-		// Take first KinD node and redirect pod hostname requests there
-		node := GetNodes(test)[0]
-		hostname := GetClusterHostname(test)
-		IP := GetNodeInternalIP(test, node)
 
-		test.T().Logf("Setting KinD cluster hostname '%s' to node IP '%s' for SDK pod", hostname, IP)
-		job.Spec.Template.Spec.HostAliases = []corev1.HostAlias{
-			{
-				IP:        IP,
-				Hostnames: []string{hostname},
+	// Create an AppWrapper resource
+	aw := &mcadv1beta1.AppWrapper{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      appwrapperName,
+			Namespace: namespace.Name,
+		},
+		Spec: mcadv1beta1.AppWrapperSpec{
+			AggrResources: mcadv1beta1.AppWrapperResourceList{
+				GenericItems: []mcadv1beta1.AppWrapperGenericResource{
+					{
+						DesiredAvailable: 1,
+						CustomPodResources: []mcadv1beta1.CustomPodResourceTemplate{
+							{
+								Replicas: 1,
+								Requests: corev1.ResourceList{
+									corev1.ResourceCPU:    resource.MustParse("250m"),
+									corev1.ResourceMemory: resource.MustParse("512Mi"),
+								},
+								Limits: corev1.ResourceList{
+									corev1.ResourceCPU:    resource.MustParse("1"),
+									corev1.ResourceMemory: resource.MustParse("1G"),
+								},
+							},
+						},
+						GenericTemplate:  Raw(test, job),
+						CompletionStatus: "Complete",
+					},
+				},
 			},
-		}
-
-		// Propagate hostname into Python code as env variable
-		hostnameEnvVar := corev1.EnvVar{Name: "CLUSTER_HOSTNAME", Value: hostname}
-		job.Spec.Template.Spec.Containers[0].Env = append(job.Spec.Template.Spec.Containers[0].Env, hostnameEnvVar)
+		},
 	}
 
-	job, err := test.Client().Core().BatchV1().Jobs(namespace.Name).Create(test.Ctx(), job, metav1.CreateOptions{})
+	_, err = test.Client().MCAD().WorkloadV1beta1().AppWrappers(namespace.Name).Create(test.Ctx(), aw, metav1.CreateOptions{})
 	test.Expect(err).NotTo(HaveOccurred())
-	test.T().Logf("Created Job %s/%s successfully", job.Namespace, job.Name)
+	test.T().Logf("Created MCAD AppWrapper %s/%s successfully", aw.Namespace, aw.Name)
 
-	test.T().Logf("Waiting for Job %s/%s to complete", job.Namespace, job.Name)
-	test.Eventually(Job(test, job.Namespace, job.Name), TestTimeoutLong).Should(
-		Or(
-			WithTransform(ConditionStatus(batchv1.JobComplete), Equal(corev1.ConditionTrue)),
-			WithTransform(ConditionStatus(batchv1.JobFailed), Equal(corev1.ConditionTrue)),
-		))
+	test.T().Logf("Waiting for MCAD %s/%s to be running", aw.Namespace, aw.Name)
+	test.Eventually(AppWrapper(test, namespace, aw.Name), TestTimeoutMedium).
+		Should(WithTransform(AppWrapperState, Equal(mcadv1beta1.AppWrapperStateActive)))
 
-	// Assert the job has completed successfully
-	test.Expect(GetJob(test, job.Namespace, job.Name)).
-		To(WithTransform(ConditionStatus(batchv1.JobComplete), Equal(corev1.ConditionTrue)))
 }
 
-// Submit a Job to the Ray cluster and trains the MNIST dataset using the CodeFlare SDK.
-func TestMnistJobSubmit(t *testing.T) {
-
+func TestMNISTCheckAppWrapperStatus(t *testing.T) {
 	test := With(t)
 	test.T().Parallel()
 
+	// get namespace
 	namespace := GetNamespaceWithName(test, namespaceName)
 
 	//delete the namespace after test complete
 	defer DeleteTestNamespace(test, namespace)
 
-	// Test configuration
-	config := CreateConfigMap(test, namespace.Name, map[string][]byte{
-		// SDK script
-		"mnist_rayjob.py": ReadFile(test, "mnist_rayjob.py"),
-		// pip requirements
-		"requirements.txt": ReadFile(test, "mnist_pip_requirements.txt"),
-		// MNIST training script
-		"mnist.py": ReadFile(test, "mnist.py"),
-		// codeflare-sdk installation script
-		"install-codeflare-sdk.sh": ReadFile(test, "install-codeflare-sdk.sh"),
-	})
-
-	// Create RBAC, retrieve token for user with limited rights
-	policyRules := []rbacv1.PolicyRule{
-		{
-			Verbs:     []string{"get", "create", "delete", "list", "patch", "update"},
-			APIGroups: []string{mcadv1beta1.GroupName},
-			Resources: []string{"appwrappers"},
-		},
-		{
-			Verbs:     []string{"get", "list"},
-			APIGroups: []string{rayv1.GroupVersion.Group},
-			Resources: []string{"rayclusters", "rayclusters/status"},
-		},
-		{
-			Verbs:     []string{"get", "list"},
-			APIGroups: []string{"route.openshift.io"},
-			Resources: []string{"routes"},
-		},
-		{
-			Verbs:     []string{"get", "list"},
-			APIGroups: []string{"networking.k8s.io"},
-			Resources: []string{"ingresses"},
-		},
-	}
-
-	// Create cluster wide RBAC, required for SDK OpenShift check
-	// TODO reevaluate once SDK change OpenShift detection logic
-	clusterPolicyRules := []rbacv1.PolicyRule{
-		{
-			Verbs:         []string{"get", "list"},
-			APIGroups:     []string{"config.openshift.io"},
-			Resources:     []string{"ingresses"},
-			ResourceNames: []string{"cluster"},
-		},
-	}
-
-	serviceAccount := CreateServiceAccount(test, namespace.Name)
-	role := CreateRole(test, namespace.Name, policyRules)
-	CreateRoleBinding(test, namespace.Name, serviceAccount, role)
-	clusterRole := CreateClusterRole(test, clusterPolicyRules)
-	CreateClusterRoleBinding(test, serviceAccount, clusterRole)
-
-	job := &batchv1.Job{
-		TypeMeta: metav1.TypeMeta{
-			APIVersion: batchv1.SchemeGroupVersion.String(),
-			Kind:       "Job",
-		},
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "rayjob",
-			Namespace: namespaceName,
-		},
-		Spec: batchv1.JobSpec{
-			Completions:  Ptr(int32(1)),
-			Parallelism:  Ptr(int32(1)),
-			BackoffLimit: Ptr(int32(0)),
-			Template: corev1.PodTemplateSpec{
-				Spec: corev1.PodSpec{
-					Containers: []corev1.Container{
-						{
-							Name: "test",
-							// FIXME: switch to base Python image once the dependency on OpenShift CLI is removed
-							// See https://github.com/project-codeflare/codeflare-sdk/pull/146
-							Image:   "quay.io/opendatahub/notebooks:jupyter-minimal-ubi8-python-3.8-4c8f26e",
-							Command: []string{"/bin/sh", "-c", "cp /test/* . && chmod +x install-codeflare-sdk.sh && ./install-codeflare-sdk.sh && python mnist_rayjob.py" + " " + namespaceName},
-							VolumeMounts: []corev1.VolumeMount{
-								{
-									Name:      "test",
-									MountPath: "/test",
-								},
-								{
-									Name:      "codeflare-sdk",
-									MountPath: "/codeflare-sdk",
-								},
-								{
-									Name:      "workdir",
-									MountPath: "/workdir",
-								},
-							},
-							WorkingDir: "/workdir",
-							SecurityContext: &corev1.SecurityContext{
-								AllowPrivilegeEscalation: Ptr(false),
-								SeccompProfile: &corev1.SeccompProfile{
-									Type: "RuntimeDefault",
-								},
-								Capabilities: &corev1.Capabilities{
-									Drop: []corev1.Capability{"ALL"},
-								},
-								RunAsNonRoot: Ptr(true),
-							},
-						},
-					},
-					Volumes: []corev1.Volume{
-						{
-							Name: "test",
-							VolumeSource: corev1.VolumeSource{
-								ConfigMap: &corev1.ConfigMapVolumeSource{
-									LocalObjectReference: corev1.LocalObjectReference{
-										Name: config.Name,
-									},
-								},
-							},
-						},
-						{
-							Name: "codeflare-sdk",
-							VolumeSource: corev1.VolumeSource{
-								EmptyDir: &corev1.EmptyDirVolumeSource{},
-							},
-						},
-						{
-							Name: "workdir",
-							VolumeSource: corev1.VolumeSource{
-								EmptyDir: &corev1.EmptyDirVolumeSource{},
-							},
-						},
-					},
-					RestartPolicy:      corev1.RestartPolicyNever,
-					ServiceAccountName: serviceAccount.Name,
-				},
-			},
-		},
-	}
-
-	if GetClusterType(test) == KindCluster {
-		// Take first KinD node and redirect pod hostname requests there
-		node := GetNodes(test)[0]
-		hostname := GetClusterHostname(test)
-		IP := GetNodeInternalIP(test, node)
-
-		test.T().Logf("Setting KinD cluster hostname '%s' to node IP '%s' for SDK pod", hostname, IP)
-		job.Spec.Template.Spec.HostAliases = []corev1.HostAlias{
-			{
-				IP:        IP,
-				Hostnames: []string{hostname},
-			},
-		}
-
-		// Propagate hostname into Python code as env variable
-		hostnameEnvVar := corev1.EnvVar{Name: "CLUSTER_HOSTNAME", Value: hostname}
-		job.Spec.Template.Spec.Containers[0].Env = append(job.Spec.Template.Spec.Containers[0].Env, hostnameEnvVar)
-	}
-
-	job, err := test.Client().Core().BatchV1().Jobs(namespaceName).Create(test.Ctx(), job, metav1.CreateOptions{})
+	aw, err := test.Client().MCAD().WorkloadV1beta1().AppWrappers(namespace.Name).Get(test.Ctx(), appwrapperName, metav1.GetOptions{})
 	test.Expect(err).NotTo(HaveOccurred())
-	test.T().Logf("Created Job %s/%s successfully", job.Namespace, job.Name)
 
+	job, err := test.Client().Core().BatchV1().Jobs(namespace.Name).Get(test.Ctx(), jobName, metav1.GetOptions{})
+	test.Expect(err).NotTo(HaveOccurred())
 	test.T().Logf("Waiting for Job %s/%s to complete", job.Namespace, job.Name)
-	test.Eventually(Job(test, job.Namespace, job.Name), TestTimeoutLong).Should(
+	test.Eventually(AppWrapper(test, namespace, aw.Name), TestTimeoutLong).Should(
 		Or(
-			WithTransform(ConditionStatus(batchv1.JobComplete), Equal(corev1.ConditionTrue)),
-			WithTransform(ConditionStatus(batchv1.JobFailed), Equal(corev1.ConditionTrue)),
+			WithTransform(AppWrapperState, Equal(mcadv1beta1.AppWrapperStateCompleted)),
+			WithTransform(AppWrapperState, Equal(mcadv1beta1.AppWrapperStateFailed)),
 		))
 
 	// Assert the job has completed successfully
-	test.Expect(GetJob(test, job.Namespace, job.Name)).
-		To(WithTransform(ConditionStatus(batchv1.JobComplete), Equal(corev1.ConditionTrue)))
+	test.Expect(GetAppWrapper(test, namespace, aw.Name)).
+		To(WithTransform(AppWrapperState, Equal(mcadv1beta1.AppWrapperStateCompleted)))
 
 }
